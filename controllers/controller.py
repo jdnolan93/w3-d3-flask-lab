5c4afa4e9ef260e5b454e5f22340201b1f75d017
Jamie <jamienolan93@hotmail.co.uk>
2021-12-01 17:10:56 +0000

    Added extension but it isn't working

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,9 +1,13 @@
 from app import app
 from flask import render_template
+from models import order
 from models.order_list import orders
 
 @app.route('/orders')
 def index():
     return render_template('index.html', title='Orders', orders=orders)
 
-
+@app.route('/orders/<index>')
+def specific_order(index):
+    order = orders[int(index)]
+    return render_template('order.html', title='Specific', order=order)
