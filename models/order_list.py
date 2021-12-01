cd07293e793be4dfbd2384228a758ce2312c4394
Jamie <jamienolan93@hotmail.co.uk>
2021-12-01 14:33:53 +0000

    improved HTML

--- models/order_list.py
+++ models/order_list.py
@@ -1,5 +1,5 @@
 from models.order import *
 
 order1 = Order("Jamie Nolan", "30th of March", 5, "Nike ZoomX VaporflyNEXT%", 46, 300.00)
-order2 = Order("James Dolan", "31st of March", 1, "Nike Zoom Fly 3", 45, 120.00)
+order2 = Order("James Dolan", "31st of March", 2, "Nike Zoom Fly 3", 45, 120.00)
 orders = [order1, order2]
